0b2f468895591b771dfc3c19acf6230249a32d4f
evacougnon <eva.cougnon@utas.edu.au>
2020-05-22 14:39:44 +1000

    more cleansing

--- AR1/gen_1deg_res.py
+++
@@ -1,5 +0,0 @@
-'''
-	from the SSTa_month.nc, average per 1 degree bin
-	the SSTa to match the HadISST. Also easier to deal with 
-	EOF analyses, as we have to subsample at 1 degree, 
-	better to get a 1 degree product
